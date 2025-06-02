f0be275aaa61324b0590aeaaf6f69e38d4622b6e
anees028 <rehmananees028@gmail.com>
2025-06-03 01:08:32 +0200

    Data added for the rendering of multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header";
+import {CORE_CONCEPTS} from './data/data'
 
 function App() {
+
+  const coreData = CORE_CONCEPTS;
+
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts" >
+          <h2>Core Concept</h2>
+          <ul>
+            {coreData.map((item, index) => (
+              <CoreConcept  id={index} title={item.title} description={item.description} image={item.image}/>
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
